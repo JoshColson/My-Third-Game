83e4e519045414b41628218a5a85250187410850
joshskinner1 <74775688+joshskinner1@users.noreply.github.com>
2020-11-20 13:33:59 +0000

    Add files via upload

---
+++ DeathCount.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+
+public class DeathCount : MonoBehaviour {
+
+    public Text DeathText;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        DeathText.text = ("Deaths: ") + GameManager.Deathcount.ToString();
+
+    }
+}
